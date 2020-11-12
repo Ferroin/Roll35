8e63434bfd147291e07af68059476a1c461273e2
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2020-11-12 15:39:25 -0500

    Version 1.2.0 release.

--- apps/roll35_core/mix.exs
+++ apps/roll35_core/mix.exs
@@ -4,7 +4,7 @@ defmodule Roll35Core.MixProject do
   def project do
     [
       app: :roll35_core,
-      version: "1.0.2",
+      version: "2.0.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
