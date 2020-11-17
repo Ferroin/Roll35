3bfd72067fe5c30285690c8985e70a2dddaa3677
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2020-11-17 14:03:10 -0500

    Version 2.0.0 release.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Roll35.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "1.4.0",
+      version: "2.0.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "Roll35",
