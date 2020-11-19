00a905f2c7b56f18024a8bd32a790be840ca1f33
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2020-11-19 09:16:28 -0500

    Version 2.1.0 release.

--- apps/roll35_bot/mix.exs
+++ apps/roll35_bot/mix.exs
@@ -4,7 +4,7 @@ defmodule Roll35Bot.MixProject do
   def project do
     [
       app: :roll35_bot,
-      version: "4.0.2",
+      version: "4.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
