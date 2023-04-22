6beb1009800b66feafed985fdd43091053a03482
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-04-22 08:42:24 -0400

    Add command to query version information.

--- roll35/core.py
+++ roll35/core.py
@@ -1,10 +1,15 @@
 '''Core commands.'''
 
 import logging
+import os
+import platform
+
+import nextcord
 
 from nextcord.ext import commands
 
 from .cog import Cog
+from .common import VERSION
 
 logger = logging.getLogger(__name__)
 
@@ -17,3 +22,18 @@ class Core(Cog):
     async def ping(self, ctx):
         '''Check if the bot is alive'''
         await ctx.send('pong')
+
+    @commands.command()
+    async def version(self, ctx):
+        '''Check the version of the bot'''
+        if '_R35_CONTAINER_BUILD_DATE' in os.environ:
+            build_date = os.environ['_R35_CONTAINER_BUILD_DATE']
+        else:
+            build_date = 'unknown'
+
+        await ctx.send(
+            f'\nRoll35 version: { VERSION[0] }.{ VERSION[1] }.{ VERSION[2] }\n' +
+            f'Image build date: { build_date }\n' +
+            f'Running on Python { platform.python_version() } ({ platform.python_implementation() })\n' +
+            f'Using Nextcord { nextcord.__version__ }'
+        )
