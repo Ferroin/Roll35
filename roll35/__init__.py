86fd691d7b7564fde65f4b6fab45d4ca54a4def6
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-04-22 09:04:13 -0400

    Restructure to allow running module directly as bot.

--- roll35/__init__.py
+++ roll35/__init__.py
@@ -1,26 +1,13 @@
 # Copyright (c) 2023 Austin S. Hemmelgarn
 # SPDX-License-Identifier: MITNFA
 
-import asyncio
-import concurrent.futures
-import logging
-import sys
-
-from concurrent.futures import ProcessPoolExecutor
-
-import nextcord
-
-from nextcord.ext import commands
-
-from .common import VERSION, prepare_cog, did_you_mean
-
 from .core import Core
 from .spell import Spell
 from .magicitem import MagicItem
 from .armor import Armor
 from .weapon import Weapon
 
-from .renderer import Renderer
+from .common import VERSION
 
 COGS = [
     Core,
@@ -29,8 +16,6 @@ COGS = [
     Armor,
     Weapon,
 ]
-
-POOL = ProcessPoolExecutor()
 
 BOT_HELP = '''Roll items and spells for first-edition Pathfinder.
 
@@ -49,77 +34,3 @@ Supported commands, grouped by category:
 '''
 
 __version__ = f'{ VERSION[0] }.{ VERSION[1] }.{ VERSION[2] }'
-
-logger = logging.getLogger(__name__)
-
-
-async def register_cogs(bot, renderer):
-    '''Register all defined cogs on the specified bot.'''
-    loaders = [
-        renderer.load_data(),
-    ]
-
-    for entry in COGS:
-        if not bot.get_cog(entry.qualified_name):
-            logger.info(f'Loading Cog: { entry }')
-            cog = entry(bot, POOL, renderer)
-            loaders.append(prepare_cog(cog))
-            bot.add_cog(cog)
-
-    if loaders:
-        await asyncio.gather(*loaders)
-
-
-class Bot(commands.Bot):
-    async def on_command_error(self, ctx, exception):
-        if isinstance(exception, commands.errors.CommandNotFound):
-            loop = asyncio.get_running_loop()
-
-            match await loop.run_in_executor(
-                POOL,
-                did_you_mean,
-                [x.name for x in self.walk_commands()],
-                exception.command_name,
-                True,
-            ):
-                case (True, msg):
-                    return await ctx.send(f'{ exception.command_name } is not a recognized command. { msg }')
-                case (False, _):
-                    return await ctx.send(f'{ exception.command_name } is not a recognized command.')
-
-        await super().on_command_error(ctx, exception)
-
-    async def on_error(self, event_method, *args, **kwargs):
-        match sys.exc_info():
-            case (concurrent.futures.BrokenExecutor, _, _):
-                raise
-            case _:
-                await super().on_error(event_method, *args, **kwargs)
-
-
-def main(token):
-    intents = nextcord.Intents.default()
-    intents.message_content = True
-
-    bot = Bot(
-        case_insensitive=True,
-        command_prefix=[
-            '/r35 ',
-            '/R35 ',
-        ],
-        description=BOT_HELP,
-        intents=intents,
-        strip_after_prefix=True,
-    )
-    renderer = Renderer(POOL, bot)
-
-    @bot.event
-    async def on_ready():
-        logger.info(f'Successfully logged in as { bot.user }')
-
-    @bot.event
-    async def on_connect():
-        await register_cogs(bot, renderer)
-
-    logger.info(f'Starting bot with token: { token }')
-    bot.run(token)
