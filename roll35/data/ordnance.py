968e62b2fb4be2cccd0c170de2ada2aa084f1dc2
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-04-27 13:04:37 -0400

    General cleanup of roll35.data.agent.Agent.

--- roll35/data/ordnance.py
+++ roll35/data/ordnance.py
@@ -4,10 +4,11 @@
 '''Data handling for armor, shields, and weapons.'''
 
 import logging
+import random
 
 from . import agent
 from . import types
-from ..common import DATA_ROOT, yaml, make_weighted_entry, check_ready
+from ..common import DATA_ROOT, yaml, make_weighted_entry, check_ready, norm_string, did_you_mean
 
 logger = logging.getLogger(__name__)
 
@@ -140,6 +141,13 @@ def create_xform(base, enchantments, specific):
     return xform
 
 
+def generate_tags_entry(items):
+    '''Generate a list of tags based on a list of items.'''
+    tags = map(lambda x: set(x['tags']), items)
+    tags = set.union(*tags)
+    return tags | {x['type'] for x in items}
+
+
 class OrdnanceAgent(agent.Agent):
     def __init__(self, dataset, pool, name, logger=logger):
         super().__init__(dataset, pool, name, logger)
@@ -169,7 +177,7 @@ class OrdnanceAgent(agent.Agent):
         )
 
         ret['base'] = types.R35List(data['base'])
-        ret['tags'] = agent.generate_tags_entry(data['base'])
+        ret['tags'] = generate_tags_entry(data['base'])
         ret['enchantments'] = enchantments
         ret['specific'] = specific
         ret['masterwork'] = data['masterwork']
@@ -178,8 +186,143 @@ class OrdnanceAgent(agent.Agent):
         return ret
 
     async def random(self, **kwargs):
-        return await super().random_pattern(**kwargs)
+        return await self.random_pattern(**kwargs)
+
+    @check_ready
+    @agent.ensure_costs
+    async def random_pattern(self, rank, subrank, allow_specific=True, mincost=None, maxcost=None):
+        match rank:
+            case None:
+                rank = random.choice(types.RANK)
+            case rank if self._valid_rank(rank):
+                pass
+            case _:
+                raise ValueError(f'Invalid rank for { self.name }: { rank }')
+
+        if subrank is None:
+            subrank = random.choice(types.SUBRANK)
+
+        items = agent.costfilter(self._data[rank][subrank], mincost, maxcost)
+
+        if allow_specific:
+            return random.choice(items)['value']
+        else:
+            return random.choice(list(filter(
+                lambda x: 'specific' not in x['value'],
+                items
+            )))['value']
+
+    @check_ready
+    async def get_base(self, name):
+        items = self._data['base']
+        norm_name = norm_string(name)
+
+        match next((x for x in items if norm_string(x['name']) == norm_name), None):
+            case None:
+                match await self._process_async(
+                    did_you_mean,
+                    [items, norm_name],
+                ):
+                    case (True, msg):
+                        return (
+                            False,
+                            f'{ name } is not a recognized item.\n { msg }'
+                        )
+                    case (False, msg):
+                        return (False, msg)
+            case item:
+                return (True, item)
+
+    @check_ready
+    async def random_base(self, tags=[]):
+        items = self._data['base']
+
+        match tags:
+            case []:
+                pass
+            case [*tags]:
+                items = list(filter(
+                    lambda x: all(
+                        map(
+                            lambda y: y == x['type'] or y in x['tags'],
+                            tags
+                        )
+                    ), items
+                ))
+            case _:
+                raise ValueError('Tags must be a list.')
+
+        if items:
+            return random.choice(items)
+        else:
+            return None
+
+    @check_ready
+    async def random_enchant(self, group, bonus, enchants=[], tags=[]):
+        items = self._data['enchantments'][group][bonus]
+
+        def _efilter(x):
+            result = True
+
+            match x:
+                case {'exclude': excluded}:
+                    result = result and not any(lambda y: y in excluded, enchants)
+
+            match x:
+                case {'limit': {'only': limit}}:
+                    result = result and any(lambda y: y in limit, tags)
+                case {'limit': {'not': limit}}:
+                    result = result and not any(lambda y: y in limit, tags)
+
+            return result
+
+        match list(filter(_efilter, items)):
+            case []:
+                return None
+            case [*opts]:
+                return random.choice(opts)['value']
+
+    @check_ready
+    @agent.ensure_costs
+    async def random_specific(self, *args, mincost=None, maxcost=None):
+        match args:
+            case [_, _, _]:
+                group = args[0]
+                rank = args[1]
+                subrank = args[2]
+            case [_, _]:
+                group = False
+                rank = args[0]
+                subrank = args[1]
+
+        match rank:
+            case None:
+                rank = random.choice(types.RANK)
+            case rank if self._valid_rank(rank):
+                pass
+            case _:
+                raise ValueError(f'Invalid rank for { self.name }: { rank }')
+
+        if subrank is None:
+            subrank = random.choice(types.SUBRANK)
+
+        items = self._data['specific']
+
+        if group:
+            if group not in items:
+                raise ValueError(f'Unrecognized item type for { self.name }: { rank }')
+
+            return random.choice(items[group][rank][subrank])['value']
+        else:
+            return random.choice(items[rank][subrank])['value']
 
     @check_ready
     async def get_bonus_costs(self, base):
         return get_enchant_bonus_costs(self._data, base)
+
+    @check_ready
+    async def tags(self):
+        if 'tags' in self._data:
+            return list(self._data['tags'])
+        else:
+            return []
