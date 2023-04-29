704463f4df31d261e9c605f710b3f7f1af69da15
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-04-29 12:56:05 -0400

    Tidy-up module initialization.
    - Use `__all__` to get submodules properly.
    - Explicitly check for Python 3.10 or newer.

--- roll35/__init__.py
+++ roll35/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2023 Austin S. Hemmelgarn
 # SPDX-License-Identifier: MITNFA
 
+import sys
+
 from .common import VERSION
 
 BOT_HELP = '''Roll items and spells for first-edition Pathfinder.
@@ -22,3 +24,16 @@ Supported commands, grouped by category:
 '''
 
 __version__ = f'{ VERSION[0] }.{ VERSION[1] }.{ VERSION[2] }'
+
+__all__ = [
+    'cogs',
+    'common',
+    'data',
+    'parser',
+    'renderer',
+    'retcode',
+    'types',
+]
+
+if not sys.version_info >= (3, 10):
+    raise RuntimeError('roll35 requires Python 3.10 or newer.')
