cd5624a372324214aab74fe2b15ddc364f21769d
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-06-14 10:14:17 -0400

    Add sync API to ranked and compound data agents.

--- roll35/data/compound.py
+++ roll35/data/compound.py
@@ -14,7 +14,7 @@ from .classes import ClassesAgent
 from .spell import SpellAgent
 from .. import types
 from ..common import yaml, bad_return, ismapping, rnd, flatten, make_weighted_entry
-from ..log import log_call_async
+from ..log import log_call_async, log_call
 
 if TYPE_CHECKING:
     from collections.abc import Mapping, Sequence, Iterable
@@ -85,9 +85,32 @@ class CompoundAgent(agent.Agent):
             compound=process_compound_itemlist(data, classes, extra_classes)
         )
 
+    def load_data(self: CompoundAgent, /) -> types.Ret:
+        '''Load data for this agent.'''
+        if not self.ready:
+            logger.info('Fetching class data.')
+
+            classes = cast(ClassesAgent, self._ds['classes']).classdata()
+            extra_classes = cast(SpellAgent, self._ds['spell']).EXTRA_CLASS_NAMES
+
+            if classes == types.Ret.NOT_READY:
+                raise RuntimeError('Class data is not ready.')
+
+            logger.info(f'Loading { self.name } data.')
+
+            with open(self._ds.src / f'{ self.name }.yaml') as f:
+                data = yaml.load(f)
+
+            self._data = self._process_data(data, classes, extra_classes)
+            logger.info(f'Finished loading { self.name } data.')
+
+            self.ready = True
+
+        return types.Ret.OK
+
     async def load_data_async(self: CompoundAgent, pool: Executor, /) -> types.Ret:
         '''Load data for this agent.'''
-        if not self._ready.is_set():
+        if not self.ready:
             logger.info('Fetching class data.')
 
             classes = await cast(ClassesAgent, self._ds['classes']).W_classdata_async()
@@ -101,9 +124,117 @@ class CompoundAgent(agent.Agent):
             self._data = await self._process_async(pool, self._process_data, [data, classes, extra_classes])
             logger.info(f'Finished loading { self.name } data.')
 
-            self._ready.set()
+            self.ready = True
 
         return types.Ret.OK
+
+    def __handle_level_search(
+            self: CompoundAgent,
+            /,
+            rank: types.Rank | None = None,
+            *,
+            level: int,
+            mincost: types.item.Cost | None = None,
+            maxcost: types.item.Cost | None = None) -> \
+            types.CompoundItem | types.item.CompoundSpellItem | types.Ret:
+        if self._data.compound is None:
+            return types.Ret.NO_MATCH
+
+        match rank:
+            case None:
+                searchitems: Iterable[types.CompoundItem] = \
+                    flatten(cast(Sequence[Sequence[types.CompoundItem]], self._data.compound.values()))
+            case rank if self._valid_rank(rank):
+                searchitems = cast(Sequence[types.CompoundItem], self._data.compound[rank])
+            case _:
+                raise ValueError(f'Invalid rank for { self.name }: { rank }')
+
+        possible = []
+
+        for i1 in searchitems:
+            match i1:
+                case types.item.CompoundSpellItem(spell={'level': int() as l1}) if l1 == level:
+                    possible.append(i1)
+
+        items = cast(Sequence[types.item.CompoundSpellItem], agent.costfilter(possible, mincost=mincost, maxcost=maxcost))
+
+        match rnd(items):
+            case types.Ret.NO_MATCH:
+                return types.Ret.NO_MATCH
+            case i2:
+                return i2
+
+    def __handle_item(
+            self: CompoundAgent,
+            /,
+            item: types.CompoundItem | types.item.CompoundSpellItem | types.Ret,
+            spell: types.Result[types.item.Spell] | types.Ret | None) -> \
+            types.CompoundItem | types.item.CompoundSpellItem | types.Ret:
+        match item:
+            case types.Ret.NO_MATCH:
+                return types.Ret.NO_MATCH
+            case types.item.CompoundSpellItem():
+                match spell:
+                    case types.Ret.NOT_READY:
+                        return types.Ret.NOT_READY
+                    case (types.Ret.OK, types.item.Spell() as s1):
+                        if hasattr(item, 'costmult') and item.costmult is not None and s1.rolled_caster_level is not None:
+                            item.cost = item.costmult * s1.rolled_caster_level
+
+                        item.rolled_spell = s1
+                        return item
+                    case (types.Ret() as r1, msg) if r1 is not types.Ret.OK:
+                        logger.warning(f'Failed to roll random spell for item using parameters: { spell }, recieved: { msg }')
+                        return r1
+                    case ret:
+                        logger.error(bad_return(ret))
+                        return types.Ret.FAILED
+            case types.CompoundItem() as item:
+                return item
+            case ret:
+                logger.warning(bad_return(ret))
+                return types.Ret.FAILED
+
+        # The below line should never actually be run, as the above match clauses are (theoretically) exhaustive.
+        #
+        # However, mypy thinks this function is missing a return statement, and this line convinces it otherwise.
+        raise RuntimeError
+
+    @log_call(logger, 'roll compound item')
+    def random(
+            self: CompoundAgent,
+            /,
+            rank: types.Rank | None = None,
+            *,
+            cls: str | None = None,
+            level: int | None = None,
+            mincost: types.item.Cost | None = None,
+            maxcost: types.item.Cost | None = None) -> \
+            types.CompoundItem | types.item.CompoundSpellItem | types.Ret:
+        '''Roll a random item, then roll a spell for it if needed.'''
+        match level:
+            case None:
+                item: types.CompoundItem | types.item.CompoundSpellItem | types.Ret = cast(types.CompoundItem, super().random_compound(
+                    rank=rank,
+                    mincost=mincost,
+                    maxcost=maxcost,
+                ))
+            case int():
+                item = self.__handle_level_search(rank=rank, level=level, mincost=mincost, maxcost=maxcost)
+            case _:
+                raise ValueError
+
+        spell = None
+
+        if isinstance(item, types.item.CompoundSpellItem):
+            spellparams = item.spell
+
+            if spellparams.cls is None and cls is not None:
+                spellparams.cls = cls
+
+            spell = cast(SpellAgent, self._ds['spell']).random(**spellparams.dict())
+
+        return self.__handle_item(item, spell)
 
     @log_call_async(logger, 'roll compound item')
     async def random_async(
@@ -125,64 +256,18 @@ class CompoundAgent(agent.Agent):
                     maxcost=maxcost,
                 ))
             case int():
-                if self._data.compound is None:
-                    return types.Ret.NO_MATCH
-
-                match rank:
-                    case None:
-                        searchitems: Iterable[types.CompoundItem] = \
-                            flatten(cast(Sequence[Sequence[types.CompoundItem]], self._data.compound.values()))
-                    case rank if self._valid_rank(rank):
-                        searchitems = cast(Sequence[types.CompoundItem], self._data.compound[rank])
-                    case _:
-                        raise ValueError(f'Invalid rank for { self.name }: { rank }')
-
-                possible = []
-
-                for i1 in searchitems:
-                    match i1:
-                        case types.item.CompoundSpellItem(spell={'level': int() as l1}) if l1 == level:
-                            possible.append(i1)
-
-                items = cast(Sequence[types.item.CompoundSpellItem], agent.costfilter(possible, mincost=mincost, maxcost=maxcost))
-
-                match rnd(items):
-                    case types.Ret.NO_MATCH:
-                        return types.Ret.NO_MATCH
-                    case i2:
-                        item = i2
+                item = self.__handle_level_search(rank=rank, level=level, mincost=mincost, maxcost=maxcost)
             case _:
                 raise ValueError
 
-        match item:
-            case types.Ret.NO_MATCH:
-                return types.Ret.NO_MATCH
-            case types.item.CompoundSpellItem(spell=spell):
-                if spell.cls is None and cls is not None:
-                    spell.cls = cls
+        spell = None
 
-                match await cast(SpellAgent, self._ds['spell']).random_async(**spell.dict()):
-                    case types.Ret.NOT_READY:
-                        return types.Ret.NOT_READY
-                    case (types.Ret.OK, types.item.Spell() as s1):
-                        if hasattr(item, 'costmult') and item.costmult is not None and s1.rolled_caster_level is not None:
-                            item.cost = item.costmult * s1.rolled_caster_level
+        if isinstance(item, types.item.CompoundSpellItem):
+            spellparams = item.spell
 
-                        item.rolled_spell = s1
-                        return item
-                    case (types.Ret() as r1, msg) if r1 is not types.Ret.OK:
-                        logger.warning(f'Failed to roll random spell for item using parameters: { spell }, recieved: { msg }')
-                        return r1
-                    case ret:
-                        logger.error(bad_return(ret))
-                        return types.Ret.FAILED
-            case types.item.CompoundItem() as item:
-                return item
-            case ret:
-                logger.warning(bad_return(ret))
-                return types.Ret.FAILED
+            if spellparams.cls is None and cls is not None:
+                spellparams.cls = cls
 
-        # The below line should never actually be run, as the above match clauses are (theoretically) exhaustive.
-        #
-        # However, mypy thinks this function is missing a return statement, and this line convinces it otherwise.
-        raise RuntimeError
+            spell = await cast(SpellAgent, self._ds['spell']).random_async(**spellparams.dict())
+
+        return self.__handle_item(item, spell)
