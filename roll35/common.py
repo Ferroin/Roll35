6078f27a11bcbd37565d96922bcc644778edf133
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-06-24 18:34:46 -0400

    Bump version to 6.0.1.

--- roll35/common.py
+++ roll35/common.py
@@ -20,7 +20,7 @@ from ruamel.yaml import YAML
 from . import types
 from .log import log_call
 
-VERSION = (6, 0, 0)
+VERSION = (6, 0, 1)
 
 yaml = YAML(typ='safe')
 logger = logging.getLogger(__name__)
