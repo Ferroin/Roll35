fb95469b6fb6e95eb9f1f48953210cb77e3b0f8b
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-06-26 11:28:47 -0400

    Assorted cleanup in roll35.cogs.

--- roll35/cogs/spell.py
+++ roll35/cogs/spell.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2023 Austin S. Hemmelgarn
 # SPDX-License-Identifier: MITNFA
 
+'''Cog for rolling random spells.'''
+
 from __future__ import annotations
 
 import asyncio
@@ -48,7 +50,7 @@ SPELL_PARSER = Parser({
     'count': ParserEntry(
         type=int,
         names=[
-            'cost',
+            'count',
             'co',
             'number',
             'num',
@@ -62,7 +64,7 @@ logger = logging.getLogger(__name__)
 
 class Spell(R35Cog):
     '''Roll35 cog for handling spells.'''
-    async def _spell(self: Spell, ctx: commands.Context, *args: str) -> None:
+    async def __spell(self: Spell, ctx: commands.Context, *args: str) -> None:
         match SPELL_PARSER.parse(' '.join(args)):
             case (Ret.FAILED, msg):
                 await ctx.send(
@@ -101,18 +103,18 @@ class Spell(R35Cog):
                                 case (Ret.OK, str() as msg):
                                     results.append(msg)
                                 case (r1, msg) if r1 is not Ret.OK:
-                                    results.append(f'\nFailed to generate remaining items: { msg }')
+                                    results.append(f'\nFailed to generate remaining spells: { msg }')
                                     break
                                 case r2:
                                     logger.error(bad_return(r2))
-                                    results.append('\nFailed to generate remaining items: Unknown internal error.')
+                                    results.append('\nFailed to generate remaining spells: Unknown internal error.')
                                     break
                         case (r3, msg) if r3 is not Ret.OK:
-                            results.append(f'\nFailed to generate remaining items: { msg }')
+                            results.append(f'\nFailed to generate remaining spells: { msg }')
                             break
                         case r4:
                             logger.error(bad_return(r4))
-                            results.append('\nFailed to generate remaining items: Unknown internal error.')
+                            results.append('\nFailed to generate remaining spells: Unknown internal error.')
                             break
 
                 await ctx.trigger_typing()
@@ -138,9 +140,9 @@ class Spell(R35Cog):
              subschool or descriptor. To list recognized tags, run
              `/r35 spelltags`.
            - `count`: Roll this many spells at once.'''
-        await self._spell(ctx, *args)
+        await self.__spell(ctx, *args)
 
-    async def _spelltags(self: Spell, ctx: commands.Context, /) -> None:
+    async def __spelltags(self: Spell, ctx: commands.Context, /) -> None:
         match await cast(SpellAgent, self.ds['spell']).tags_async():
             case Ret.NOT_READY:
                 await ctx.send(NOT_READY)
@@ -160,7 +162,7 @@ class Spell(R35Cog):
         '''List known spell tags.'''
         await self._spelltags(ctx)
 
-    async def _classes(self: Spell, ctx: commands.Context, /) -> None:
+    async def __classes(self: Spell, ctx: commands.Context, /) -> None:
         match await cast(ClassesAgent, self.ds['classes']).classes_async():
             case Ret.NOT_READY:
                 await ctx.send(NOT_READY)
@@ -185,4 +187,4 @@ class Spell(R35Cog):
     @commands.command()  # type: ignore
     async def classes(self, ctx, /):
         '''List known classes for spells.'''
-        await self._classes(ctx)
+        await self.__classes(ctx)
